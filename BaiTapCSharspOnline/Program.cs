b8339b28b6f9748784192c048d138f1b8fc9b0a3
pvtruong12 <truong12boy@gmail.com>
2025-01-13 23:45:57 +0700

    Initial commit

---
+++ BaiTapCSharspOnline/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BaiTapCSharspOnline
+{
+    class Program
+    {
+        public static class UserData
+        {
+            public static string UserName;
+            public static int ID;
+            public static int Gold;
+            public static int Point;
+
+            static UserData()
+            {
+                UserName = "Accc";
+                ID = 123;
+                Gold = 234;
+                Point = 345;
+            }
+        }
+        public class Maps
+        {
+            public int ID;
+            public string Name;
+            public static string _inGame = "Liên Minh huyển thoại";
+        }
+
+        public partial class Calculator
+        {
+            public int soA;
+            public int soB;
+        }
+        public partial class Calculator
+        {
+            public string Cong()
+            {
+                return "Dap an Cong: "+(soA + soB);
+            }public string Tru()
+            {
+                return "Dap an tru: " + (soA - soB);
+            }public string Nhan()
+            {
+                return "Dap an nhan: " + (soA * soB);
+            }public string Chia()
+            {
+                double a = (soA / soB);
+                return "Dap an Chia: " + a;
+            }
+            public override string ToString()
+            {
+                return $"{Cong()} - {Tru()} - {Nhan()} - {Chia()}";
+            }
+        }
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            switch(a)
+            {
+                case 1:
+                    //object Obja = new
+                    //{
+                    //    user = UserData.UserName,
+                    //    id = UserData.ID,
+                    //    gold = UserData.Gold,
+                    //    point = UserData.Point
+                    //};
+                    //HashSet<object> hassets = new HashSet<object>();
+                    //hassets.Add(Obja);
+
+                    Console.WriteLine($"Name: {UserData.UserName}, ID: {UserData.ID}, Gold: {UserData.Gold}, Point: {UserData.Point}");
+                    break;
+                case 2:
+                    {
+                        Maps map = new Maps() { ID = 0, Name = "truong12" };
+                        LinkedList<Maps> listMaps = new LinkedList<Maps>();
+                        listMaps.AddLast(map);
+                        foreach(Maps maps in listMaps)
+                        {
+                            if(maps != null)
+                                Console.WriteLine($"ID {maps.ID}, Name: {maps.Name}, inGame {Maps._inGame}");
+                        }
+                        break;
+                    }
+                case 3:
+                    Calculator cals = new Calculator() { soA = 12, soB = 23 };
+                    List<Calculator> list = new List<Calculator>();
+                    list.Add(cals);
+                    foreach (Calculator ca in list)
+                        Console.WriteLine(cals);
+                    break;
+            }
+        }
+    }
+}
---
+++ Lab2/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab2
+{
+    class Program
+    {
+        public static void bai1aa()
+        {
+            var a = 10;
+            Console.WriteLine("x ="+a);
+        } public static void bai1b()
+        {
+            getDeltai(10);
+            getDeltai("game");
+            getDeltai(9.5);
+            getDeltai(false);
+        }
+
+        public static void getDeltai(dynamic x)
+        {
+            Console.WriteLine("value "+ x);
+        }
+
+        public class UserData
+        {
+            public int ID;
+            public string userName;
+            public int level;
+            public override string ToString()
+            {
+                return $"ID : {ID}, Name: {userName}, Level{level}";
+            }
+        }public class userData
+        {
+            public int ID;
+            public string userName;
+            public List<float> Scores;
+            public override string ToString()
+            {
+                string x = string.Join(", ",Scores);
+                return $"ID : {ID}, Name: {userName}, Danh sách điểm: {x}";
+            }
+        }
+        public static void bai2a()
+        {
+            var userInfo = new
+            {
+                id = "2024",
+                name = "faker",
+                isPlaying = false,
+                bag = new
+                {
+                    skins = 0,
+                    cups = 20
+                }
+            };
+            Console.WriteLine($"id: { userInfo.id}");
+            Console.WriteLine($"Name: { userInfo.name}");
+            Console.WriteLine($"isPlaying: { userInfo.isPlaying}");
+            Console.WriteLine($"bag skins: { userInfo.bag.skins}");
+            Console.WriteLine($"bag cups: { userInfo.bag.cups}");
+        }
+        public static void bai2b()
+        {
+            int y = 10;
+            Action<int> AnonimousMethod = delegate (int x)
+            {
+                int sum = x + y;
+                int sub = x - y;
+                Console.WriteLine($"Sum {sum} \n Sub {sub}");
+            };
+            AnonimousMethod(5);
+        }
+        static void Main(string[] args)
+        {
+
+            int a = int.Parse(Console.ReadLine());
+            switch(a)
+            {
+                case 1:
+                    {
+                        var x = 10;
+                        Action<int> bai1a = delegate (int g)
+                        {
+                            Console.WriteLine($"x ={g}");
+                        };
+                        bai1a(x);
+                        break;
+                    }
+                case 2: bai1b();break;
+                case 3:
+                    {
+                        UserData user = new UserData() { ID = 0, userName = "truong", level = 5 };
+                        if(user != null)
+                            Console.WriteLine(user);
+                        break;
+                    }
+                case 4:bai2a();break;
+                case 5: bai2b(); break;
+                case 6:
+                    {
+
+                        userData user = new userData() { ID = 0, userName = "truong", Scores = new List<float>() { 10, 9, 8, 7.5f, 2, 1.4f } };
+                        Console.WriteLine(user);
+
+                    }break;
+            }
+        }
+    }
+}
